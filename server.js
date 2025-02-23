9f89583228a5704266b69f9eb6c4591769859fb1
suyundykovv <margulansuyundykov@gmail.com>
2025-02-23 21:21:47 +0600

    create the logic of server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
-
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'public','main.html'))
+});
 app.post('/register', (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
 
